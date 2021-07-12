1483327c6436e3bb05f1cf0de0a1c74ee404666f
Paulo Freitas <psdsfj.snf20@uea.edu.br>
2021-07-11 21:41:37 -0400

    #Update: Insert viewBinding

--- app/src/main/java/com/genda/LoginActivity.kt
+++ app/src/main/java/com/genda/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.genda
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import com.genda.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityLoginBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onResume() {
+        super.onResume()
+        binding.buttonLoginUser.setOnClickListener {
+            Toast.makeText(baseContext, "Login", Toast.LENGTH_SHORT).show()
+        }
     }
 }
