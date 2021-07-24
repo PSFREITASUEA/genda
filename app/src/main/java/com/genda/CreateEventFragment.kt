5ee925619a9278c7584ce11e347e53b8bac28889
Paulo Freitas <psdsfj.snf20@uea.edu.br>
2021-07-24 17:18:21 -0400

    #Add: Basic create event screen

---
+++ app/src/main/java/com/genda/CreateEventFragment.kt
@@ -0,0 +1,19 @@
+package com.genda
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.genda.databinding.FragmentCreateEventBinding
+
+class CreateEventFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        return FragmentCreateEventBinding.inflate(inflater, container, false).root
+    }
+}
--- app/src/main/java/com/genda/HomeFragment.kt
+++ app/src/main/java/com/genda/HomeFragment.kt
@@ -5,15 +5,28 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.genda.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
+
+    private lateinit var binding: FragmentHomeBinding
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return FragmentHomeBinding.inflate(inflater, container, false).root
+        binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        setupClickListeners()
+
+        return binding.root
+    }
+
+    private fun setupClickListeners() {
+        binding.floatingActionButtonCreateEvent.setOnClickListener {
+            findNavController().navigate(HomeFragmentDirections.actionFragmentHomeToFragmentCreateEvent())
+        }
     }
 }
